8d92b4b24f67eeab6a424acecc34272558a2d7a3
Kamilskyyy <163884971+Kamilskyyy@users.noreply.github.com>
2026-05-25 14:38:22 +0200

    Update NewsController.java

--- src/main/java/pl/edu/pwr/news/controllers/NewsController.java
+++ src/main/java/pl/edu/pwr/news/controllers/NewsController.java
@@ -16,31 +16,26 @@ public class NewsController {
         this.newsService = newsService;
     }
 
-    // 1. Everything dla usera po ID
     @GetMapping("/user/{userId}")
     public NewsResponse getUserNews(@PathVariable int userId) {
         return newsService.getNewsForUser(userId);
     }
 
-    // 2. NOWY - Everything jako GET (Opcjonalne parametry w URL)
     @GetMapping("/search/everything")
     public NewsResponse searchEverything(EverythingRequestDto request) {
         return newsService.searchEverything(request);
     }
 
-    // 3. NOWY - Top Headlines jako GET (Opcjonalne parametry w URL)
     @GetMapping("/search/top-headlines")
     public NewsResponse searchTopHeadlines(TopHeadlinesRequestDto request) {
         return newsService.searchTopHeadlines(request);
     }
 
-    // 4. NOWY - Top Headlines dla usera (Tylko 1 keyword)
     @GetMapping("/user/{userId}/top-headlines")
     public NewsResponse getUserTopHeadlines(@PathVariable int userId) {
         return newsService.getTopHeadlinesForUser(userId);
     }
 
-    // 5. NOWY - Everything dla usera posortowane po POPULARNOŚCI
     @GetMapping("/user/{userId}/popular")
     public NewsResponse getPopularUserNews(@PathVariable int userId) {
         return newsService.getPopularNewsForUser(userId);
